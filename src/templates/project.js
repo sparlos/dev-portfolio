ade951f53a28819e75050de718f0b1cd2094aa3b
Stephen Sparling <sparlingcreations@gmail.com>
2019-12-19 13:20:34 -0500

    finish desktop & responsive design of project template

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,18 +1,46 @@
 import React from "react"
 
 import { graphql } from "gatsby"
-import Img from 'gatsby-image'
+import Img from "gatsby-image"
+
+import "./project.scss"
 
 const Project = ({ data }) => {
   const post = data.markdownRemark
   const { title, image, tags } = post.frontmatter
 
+  const tagList = () =>
+    tags.split(",").map((tag, i) => {
+      return (
+        <li className="flex xl:flex-33 flex-initial mt-3 lg:ml-0 mr-6" key={tag}>
+          <span className={`text-left w-full`}>{tag}</span>
+        </li>
+      )
+    })
+
   return (
-    <div>
-      <h1 className="heading-main">{title}</h1>
-      <Img className="w-3/6" fluid={image.childImageSharp.fluid} />
-      <div>{tags}</div>
-      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+    <div className="w-full font-sans my-32 xl:px-32 text-gray-900">
+      <div className="flex flex-wrap lg:justify-start justify-center px-4">
+        <h1 className="sm:text-4xl sm:font-normal font-bold text-3xl tracking-wide font-normal lg:flex-100 sm:flex-75 flex-100 mb-4">
+          {title}
+        </h1>
+        <div className="lg:flex-50 sm:flex-75 flex-100 pr-4">
+          <Img className="w-full" fluid={image.childImageSharp.fluid} />
+        </div>
+        <div className="lg:flex-50 sm:flex-75 flex-100 lg:pl-4 lg:mt-0 mt-6 content-start flex flex-wrap justify-center lg:order-none order-last xl:px-10">
+          <h3 className="font-bold flex-100 mb-2 text-gray-700">Description</h3>
+          <div
+            className="project-text"
+            dangerouslySetInnerHTML={{ __html: post.html }}
+          />
+        </div>
+        <div className="lg:flex-50 sm:flex-75 flex-100">
+          <h3 className="mt-6 font-bold text-gray-700">Technologies Used</h3>
+          <ul className="flex flex-wrap xl:justify-between text-gray-600">
+            {tagList()}
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
