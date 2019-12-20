adb7ad3adf34983dac9bc85d142edf758527b6d4
Stephen Sparling <sparlingcreations@gmail.com>
2019-12-20 10:35:57 -0500

    remove unused pages & initial gatsby comments

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,9 +55,6 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    }
   ],
 }
