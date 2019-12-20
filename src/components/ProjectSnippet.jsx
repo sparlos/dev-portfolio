e42d3b753a105e46f87236c2c9fb846f4976bdc5
Stephen Sparling <sparlingcreations@gmail.com>
2019-12-20 10:04:29 -0500

    ProjectSnippet staggered entrance

--- src/components/ProjectSnippet.jsx
+++ src/components/ProjectSnippet.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 
 import Img from 'gatsby-image'
 import TransitionLink from "gatsby-plugin-transition-link"
+import { motion } from 'framer-motion'
 
 
 const ProjectSnippet = ({ setActiveImage, imageIndex, title, blurb, imageFluid, slug }) => {
+
+  const variants = {
+    show: (i) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.1 + ((i + 1) * 0.1),
+        type: 'spring',
+        damping: 100,
+        mass: 1
+      }
+    }),
+    hide: {
+      opacity: 0,
+      y: 30
+    }
+  }
 
   return (
     <TransitionLink
@@ -18,10 +36,17 @@ const ProjectSnippet = ({ setActiveImage, imageIndex, title, blurb, imageFluid, 
       }}
       className="relative block mb-6 lg:text-gray-600 text-gray-900 hover:text-gray-900 transition-color"
       onMouseEnter={() => { setActiveImage(imageIndex) }}
+    >
+      <motion.div
+        variants={variants}
+        initial="hide"
+        animate="show"
+        custom={imageIndex}
       >
-      <h2 className="font-bold underline text-xl lg:mb-0 mb-4">{title}</h2>
-      <Img className="lg:hidden" fluid={imageFluid} />
-      <p className="lg:mt-1 mt-6 sm:text-base text-sm">{blurb}</p>
+        <h2 className="font-bold underline text-xl lg:mb-0 mb-4">{title}</h2>
+        <Img className="lg:hidden" fluid={imageFluid} />
+        <p className="lg:mt-1 mt-6 sm:text-base text-sm">{blurb}</p>
+      </motion.div>
     </TransitionLink>
   )
 }
